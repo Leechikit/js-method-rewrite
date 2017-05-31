5e1916e2ad901fece2b4425b4fddd39a6066a777
李子杰 <leechikit0823@163.com>
2017-05-31 15:15:57 +0800

    update

--- src/js/Function/Function.prototype.apply.js
+++ src/js/Function/Function.prototype.apply.js
@@ -1,18 +1,23 @@
 /**
  * @name: Function.prototype.apply
- * @description: 	在指定 this 值和参数（参数以数组或类数组对象的形式存在）的情况下调用某个函数。
+ * @description: 在指定 this 值和参数（参数以数组或类数组对象的形式存在）的情况下调用某个函数。
  * @author: lizijie
  * @update: 2017-05-30
  */
 
 Function.prototype.apply = function(thisArg, argsArray) {
-	let arr = [];
-	if (argsArray) {
+	let context = thisArg || window;
+	let result;
+	context.fn = this;
+	if (argsArray && argsArray.length > 0) {
+		let arr = [];
 		for (let i = 0, len = argsArray.length; i < len; i++) {
 			arr.push('argsArray[' + i + ']');
 		}
+		result = eval("context.fn(" + arr + ")");
+	} else {
+		result = context.fn();
 	}
-	thisArg.fn = this;
-	eval("thisArg.fn(" + arr + ")");
-	delete thisArg.fn;
+	delete context.fn;
+	return result;
 }
