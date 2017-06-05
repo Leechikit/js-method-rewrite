fdefd53f0f518a2118786920702d47cca0ce6e0a
李子杰 <leechikit0823@163.com>
2017-06-05 11:29:19 +0800

    update

---
+++ src/js/Array/Array.prototype.push.js
@@ -0,0 +1,13 @@
+/**
+ * @name: Array.prototype.push
+ * @description: 创建一个新数组，其结果是该数组中的每个元素调用一个提供的函数。
+ * @author: lizijie
+ * @update: 2017-06-05
+ */
+
+Array.prototype.push = function(...args) {
+	let index = this.length - 1;
+	for(let i of args){
+		this[++index] = i;
+	}
+}
